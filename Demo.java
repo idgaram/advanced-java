9e133f6595e8edb81061f98baa748ad9c1a83142
IDgaram <lorys.lellouche@gmail.com>
2025-03-09 16:26:37 -0400

    anonymous inner class inline example

--- Demo.java
+++ Demo.java
@@ -1,31 +1,26 @@
-class Outer{
-    static int age = 42;
+class A{
     public void show(){
-        System.out.println("data " + age);
-
+        System.out.println("in a show");
     }
-    static class Inner{
-        public void config(){
-            System.out.println("in config, data " + age);
-        }
-    }
-    // class B{
-    //     public void config(){
-    //         System.out.println("in config");
-    //     }
-    // }
-
-
 }
+
+class B extends A{
+    public void show(){
+        System.out.println("in b show");
+
+    }
+}
+
 public class Demo{
     public static void main(String[] args) {
-    // Outer obj = new Outer();
-    // obj.show();
-        
-    Outer.Inner obj1 = new Outer.Inner();
-    obj1.config();
-    // A.B obj1 = obj.new B();
-    // obj1.config();
+        A obj = new A(){
+            public void show()
+            {
+                System.out.println("in anonymous show");
+            }
+        };
+
+        obj.show();
 
     }
 }
