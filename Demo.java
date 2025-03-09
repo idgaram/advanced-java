72235b16f94ab8496570070558f62e8d34119d24
IDgaram <lorys.lellouche@gmail.com>
2025-03-09 17:22:00 -0400

    adding static variables example

--- Demo.java
+++ Demo.java
@@ -1,14 +1,40 @@
 interface A{
-     void show();
-     void config();
+    int age = 20;
+    String message = "message";
+
+    void show();
+    void config();
 }
 // abstract class A{
 //     public abstract void show();
 //     public abstract void config();
 // }
 
+class B implements A{
+
+    @Override
+    public void show() {
+        System.out.println("in B show");
+    }
+
+    @Override
+    public void config() {
+        System.out.println("in B config");
+    }
+    
+}
+
 public class Demo{
     public static void main(String[] args) {
 
+        A obj;
+        obj = new B();
+
+        obj.show();
+        obj.config();
+
+//        obj.age = 18; cannot modify variables in interface because they are final
+
+        System.out.println(obj.age);
     }
 }
