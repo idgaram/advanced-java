8ec7bf5b5f7c95c213bb1129dadd05b0c54efd2f
IDgaram <lorys.lellouche@gmail.com>
2025-03-09 16:20:31 -0400

    using static and inner to access inner class with instance of outer

--- Demo.java
+++ Demo.java
@@ -1,22 +1,31 @@
-class A{
-    int age;
+class Outer{
+    static int age = 42;
     public void show(){
-        System.out.println("in show");
+        System.out.println("data " + age);
 
     }
-    class B{
+    static class Inner{
         public void config(){
-            System.out.println("in config");
+            System.out.println("in config, data " + age);
         }
     }
+    // class B{
+    //     public void config(){
+    //         System.out.println("in config");
+    //     }
+    // }
+
+
 }
 public class Demo{
     public static void main(String[] args) {
-    A obj = new A();
-    obj.show();
+    // Outer obj = new Outer();
+    // obj.show();
         
-    A.B obj1 = obj.new B();
+    Outer.Inner obj1 = new Outer.Inner();
     obj1.config();
+    // A.B obj1 = obj.new B();
+    // obj1.config();
 
     }
 }
