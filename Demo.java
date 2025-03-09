63518ac7fb4b9e60658dea2826a0a4da6a2766d2
IDgaram <lorys.lellouche@gmail.com>
2025-03-09 17:09:24 -0400

    example of an interface as a template for a class like abstract classes

--- Demo.java
+++ Demo.java
@@ -1,34 +1,14 @@
-abstract class A{
-    public abstract void show();
-    public abstract void config();
+interface A{
+     void show();
+     void config();
 }
-
-// class B extends A {
-
-//     @Override
-//     public void show() {
-//         System.out.println("in B show");
-//     }
-
+// abstract class A{
+//     public abstract void show();
+//     public abstract void config();
 // }
 
 public class Demo{
     public static void main(String[] args) {
-        A obj = new A(){
-
-            @Override
-            public void show() {
-                System.out.println("in anonymous abstract show");
-            }
-
-            @Override
-            public void config() {
-                System.out.println("in anonymous abstract config");
-            }
-            
-        };
-        obj.show();
-        obj.config();
 
     }
 }
