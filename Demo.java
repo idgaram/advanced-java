28c8e63cb0043c3c6bd52b8c61dd4f536243c6c3
IDgaram <lorys.lellouche@gmail.com>
2025-03-13 16:01:48 -0400

    example of a conditionnal operation with the enum

--- Demo.java
+++ Demo.java
@@ -1,19 +1,37 @@
+import java.util.Random;
+
 enum Status{
     Running, Failed, Pending, Success
 }
 public class Demo{
 
     public static void main(String[] args) {
-    int i =5;
-    Status s = Status.Failed; 
-    System.out.println(s.ordinal());
-    System.out.println(s.getClass());
 
-    Status[] all = Status.values();
-    for (Status single : all) {
-        System.out.println(single + " : " + single.ordinal());
-    }
+        Random seed = new Random();
+        
+        int randomInt = seed.nextInt(Status.values().length);
 
+        System.out.println(randomInt);
+
+
+        switch (Status.values()[randomInt]) {
+            case Running:
+                System.out.println("all good");
+                break;
+            default:
+                System.out.println("not running");
+                break;
+        }
+        
+
+            // Status s = Status.values()[randomInt];
+
+            // if(s == Status.Pending){
+            //     System.out.println("all good");
+            // }
+            // else if(s != Status.Pending){
+            //     System.out.println("no good");
+            // }
     }
    
     
