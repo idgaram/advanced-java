9b018766ef58186268b8444771fe4bb8644fb868
IDgaram <lorys.lellouche@gmail.com>
2025-03-15 12:44:17 -0400

    example of multiple catch in a throw catch exceptions block

--- Demo.java
+++ Demo.java
@@ -1,20 +1,31 @@
-
 public class Demo{ 
 
     public static void main(String[] args) {
 
-        int i = 1;
+        int i = 5 ;
+        int j = 0 ;
+        int nums[] = new int[5];
+        String str = null;
+
 
 
         
 
         try {
-            int j = 10/i;
+            System.out.println(str.length());
+            j = i/j;
             System.out.println(j);
-        } catch (Exception e) {
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
+        } catch (ArithmeticException e) {
+            System.out.println("arithmetic exception: " + e);
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("out of array bounds : " + e);
+        } catch(Exception e){
             System.out.println("error happened : " + e);
         }
 
+        System.out.println(j);
     }
 
 }
