1b59fdcb3271ce28ea1c2551bd2ab538da97cbe9
IDgaram <lorys.lellouche@gmail.com>
2025-03-15 12:54:46 -0400

    example of throw keyword with exceptions

--- Demo.java
+++ Demo.java
@@ -2,30 +2,26 @@ public class Demo{
 
     public static void main(String[] args) {
 
-        int i = 5 ;
-        int j = 0 ;
-        int nums[] = new int[5];
-        String str = null;
+        int i = 20 ;
+        int j = 0;
 
 
 
         
 
         try {
-            System.out.println(str.length());
-            j = i/j;
+            j = 18/i;
+            if (j==0) {
+                throw new ArithmeticException("i don't want to print 0");
+            }
             System.out.println(j);
-            System.out.println(nums[1]);
-            System.out.println(nums[5]);
+
         } catch (ArithmeticException e) {
             System.out.println("arithmetic exception: " + e);
-        } catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("out of array bounds : " + e);
         } catch(Exception e){
             System.out.println("error happened : " + e);
         }
 
-        System.out.println(j);
     }
 
 }
