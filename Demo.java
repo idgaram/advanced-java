031717f3e82da176d998d608610f461eb31dbf31
IDgaram <lorys.lellouche@gmail.com>
2025-03-17 17:05:27 -0400

    simple example of a try catch with a method from the java.lang.Class

--- Demo.java
+++ Demo.java
@@ -1,32 +1,36 @@
 class CustomException extends Exception{
     public CustomException(String string){
-        super(string); 
+        super(string);
     }
 }
 
 public class Demo{ 
 
     public static void main(String[] args) {
-
-        int i = 20 ;
-        int j;
-
-
+        try{
+        Class.forName("CustomException");
+        } catch(ClassNotFoundException e) {
+            System.out.println("no class.forName()");
+        }
+        // int i = 20 ;
+        // int j;
+        
+        // CustomException obj = new CustomException("a");
 
         
 
-        try {
-            j = 18/i;
-            if (j==0) {
-                throw new CustomException("i don't want to print 0");
-            }
-            System.out.println(j);
+        // try {
+        //     j = 18/i;
+        //     if (j==0) {
+        //         throw new CustomException("i don't want to print 0");
+        //     }
+        //     System.out.println(j);
 
-        } catch (CustomException e) {
-            System.out.println("in custom exception: " + e);
-        } catch(Exception e){
-            System.out.println("error happened : " + e);
-        }
+        // } catch (CustomException e) {
+        //     System.out.println("in custom exception: " + e);
+        // } catch(Exception e){
+        //     System.out.println("error happened : " + e);
+        // }
 
     }
 
