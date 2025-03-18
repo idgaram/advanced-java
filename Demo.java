1cdbd02c4d8e491d015caf1547b3595ebbb1832f
IDgaram <lorys.lellouche@gmail.com>
2025-03-18 10:06:16 -0400

    example with thread.sleep() to fake a parallelism

--- Demo.java
+++ Demo.java
@@ -1,7 +1,13 @@
 class A extends Thread{
     public void show(){
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.println("hi" );
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
     public void run(){
@@ -10,8 +16,14 @@ class A extends Thread{
 }
 class B extends Thread{
     public void show(){
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.println("hello" );
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
     public void run(){
@@ -25,7 +37,19 @@ public class Demo{
    
         A obj = new A();
         B obj2 = new B();
+
+        // obj.setPriority(Thread.MAX_PRIORITY);
+        // obj2.setPriority(Thread.MIN_PRIORITY);
+
+        System.out.println(obj.getPriority());
+        System.out.println(obj2.getPriority());
+        
         obj.start();
+        try {
+            Thread.sleep(3);
+        } catch (InterruptedException e) {
+            // e.printStackTrace();
+        }
         obj2.start();
         // obj.show(); 
         // obj2.show(); 
