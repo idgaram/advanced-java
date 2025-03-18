a8fdb1ecaf35ef5edcace04c6b1581fc4b04d15b
IDgaram <lorys.lellouche@gmail.com>
2025-03-17 23:37:10 -0400

    receiving user input and converting from string to int using buffered reader (legacy)

--- Demo.java
+++ Demo.java
@@ -1,63 +1,26 @@
-// class CustomException extends Exception{
-//     public CustomException(String string){
-//         super(string);
-//     }
-// }
 
-class A{
-    public void show() throws ClassNotFoundException
-    {
-        // try {
-            System.out.println(Class.forName("B"));
-        // } catch (ClassNotFoundException e) {
-        //     System.err.println("class was not found");
-        // }
-        // System.out.println(Class.forName("B"));
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    }
-}
+// import java.io.IOException;
+
 public class Demo{ 
 
-    static{
-        System.out.println("class loaded");
-    }
-    public static void main (String[] args){
+    public static void main (String[] args) throws NumberFormatException, IOException{
+    // int nombre = 0;
+    //     try {
+    //         nombre = System.in.read();
+    //     } catch (IOException e) {
+    //         System.out.println(e);
+    //     }
+    //     System.out.println((int) nombre);
 
-        
-
-        A obj = new A();
-        try {
-            obj.show();
-        } catch (ClassNotFoundException e) {
-            System.err.println("class was not found " + e);
-        } 
-        System.out.println("hello");
-        
-        // try{
-        // Class.forName("CustomException");
-        // } catch(ClassNotFoundException e) {
-        //     System.out.println("no class.forName()");
-        // }
-        // int i = 20 ;
-        // int j;
-        
-        // CustomException obj = new CustomException("a");
-
-        
-
-        // try {
-        //     j = 18/i;
-        //     if (j==0) {
-        //         throw new CustomException("i don't want to print 0");
-        //     }
-        //     System.out.println(j);
-
-        // } catch (CustomException e) {
-        //     System.out.println("in custom exception: " + e);
-        // } catch(Exception e){
-        //     System.out.println("error happened : " + e);
-        // }
-
+    InputStreamReader in = new InputStreamReader(System.in);
+        try (BufferedReader bf = new BufferedReader(in)) {
+            int num = Integer.parseInt(bf.readLine());
+            System.out.println(num);
+        }
     }
 
 }
