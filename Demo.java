aced4878a3326fa27c254d1415803cd91cf2659e
IDgaram <lorys.lellouche@gmail.com>
2025-03-20 09:51:51 -0400

    example of a map

--- Demo.java
+++ Demo.java
@@ -1,58 +1,33 @@
+import java.util.HashMap;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.TreeSet;
+// public static <K, V> K getKeyByValue(Map<K, V> map, V targetValue){
+//     for (Map.Entry<K, V> entry : map.entrySet()) {
+//         if(entry.getValue() == targetValue){
+//             return entry.getKey();
+//         }
+//     }
+//     return null;
+// }
 
 public class Demo{ 
 
     public static void main (String[] args) throws InterruptedException{
-   
-        Collection<Integer> col = new ArrayList<Integer>();
 
-        col.add(8);
-        col.add(4);
-        col.add(5);
-        col.add(6);
-        col.add(7);
-     
-     
-        // for (int elem : col) {
-        //     System.out.println(elem);
-        // }
-        // System.out.println(col);
+        HashMap<String, Integer> myMap = new HashMap<>();
 
-        List<Integer> myList = new ArrayList<>();
+        myMap.put("eric", 18);
+        myMap.put("angelina", 88);
+        myMap.put("andre", 17);
 
-        myList.add(5);
-        myList.add(4);
-        myList.add(8);
-        myList.add(5);
-        myList.add(1);
+        // Integer targetValue = 88;
+        // String key = getKeyByValue(myMap, targetValue);
 
-        // System.out.println(myList);
-        // System.out.println(myList.get(1));
-        // System.out.println(myList.indexOf(5));
-        
+        // System.out.println(myMap.get("angelina"));
 
-        Collection<Integer> mySet = new TreeSet<>();
 
-        mySet.add(5);
-        mySet.add(4);
-        mySet.add(8);
-        mySet.add(5);
-        mySet.add(1);
-
-        
-
-        System.out.println(mySet);
-        Iterator<Integer> value= mySet.iterator();
-        while(value.hasNext()){
-            System.out.println(value.next());
+        for (String name : myMap.keySet()){
+            System.out.println(name + " has an age of : " + myMap.get(name));
         }
-        
-
 
            };
     }
