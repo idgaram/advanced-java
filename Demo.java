19c2687185ab60dd46882c55564d77cf906188a6
IDgaram <lorys.lellouche@gmail.com>
2025-03-21 12:27:23 -0400

    comparing objects

--- Demo.java
+++ Demo.java
@@ -2,31 +2,32 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-class A {
-    public static List<Integer> lister(int targetNumber) {
-        List<Integer> result = new ArrayList<>();
-        while (targetNumber > 0) {
-            result.add(targetNumber % 10);
-            targetNumber /= 10;
-        }
+class Student implements Comparable<Student> {
+    int age;
+    String name;
 
-        Collections.reverse(result);
-
-        return result;
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Student{");
+        sb.append("age=").append(age);
+        sb.append(", name=").append(name);
+        sb.append('}');
+        return sb.toString();
     }
 
-    public static List<Integer> stringLister(int targetNumber) {
-        return String.valueOf(targetNumber) // Convert to String
-                .chars() // Get character stream
-                .mapToObj(c -> c - '0') // Convert char to integer
-                .collect(Collectors.toList()); // Collect into a list
-        // return String.valueOf(targetNumber)
-        // .chars()
-        // .mapToObj(c -> c - '0')
-        // .collect(Collectors.toList());
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 
+    public int compareTo(Student arg0) {
+        if (this.age > arg0.age) {
+            return 1;
+        } else {
+            return -1;
+        }
+        // return 0;
     }
 
 }
@@ -35,10 +36,11 @@ public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Comparator<String> stringComp = new Comparator<String>() {
+        Comparator<Student> comp = new Comparator<Student>() {
+
             @Override
-            public int compare(String t, String t1) {
-                if (t.length() < t1.length()) {
+            public int compare(Student arg0, Student arg1) {
+                if (arg0.age > arg1.age) {
                     return 1;
                 } else {
                     return -1;
@@ -47,45 +49,24 @@ public class Demo {
 
         };
 
-        List<String> stringList = new ArrayList<String>();
+        List<Student> studs = new ArrayList<>();
 
-        stringList.add("lorys");
-        stringList.add("adel");
-        stringList.add("carmene");
-        stringList.add("sophie");
+        studs.add(new Student(20, "Benoit"));
+        studs.add(new Student(27, "Carmen"));
+        studs.add(new Student(14, "Nathan"));
+        studs.add(new Student(12, "Nathan"));
+        studs.add(new Student(64, "Nathan"));
+        studs.add(new Student(4, "Nathan"));
 
-        System.out.println(stringList);
-        Collections.sort(stringList, stringComp);
-        System.out.println(stringList);
+        // Student student1 = new Student(20, "Benoit");
 
-        // Comparator<Integer> comp = new Comparator<Integer>() {
-        // public int compare(Integer i, Integer j) {
-        // if (i % 10 > j % 10)
-        // return 1;
-        // else {
-        // return -1;
-        // }
-        // };
-        // };
+        // Collections.sort(studs, comp);
+        Collections.sort(studs);
 
-        // List<Integer> myList1 = new ArrayList<>();
-        // List<Integer> myList2 = new ArrayList<>();
+        for (Object elem : studs) {
+            System.out.println(elem);
+        }
+        // System.out.println((studs));
 
-        // myList1.add(121);
-        // myList1.add(985);
-        // myList1.add(16);
-
-        // System.out.println(A.lister(5984));
-        // System.out.println(A.lister(myList1.get(0)));
-        // System.out.println(A.stringLister(myList1.get(0)));
-
-        // Collections.sort(myList1);
-        // System.out.println(myList1);
-        // Collections.sort(myList1, comp);
-        // System.out.println(myList1);
-
-        // myList2.add(412);
-        // myList2.add(47856);
-        // myList2.add(2254);
     }
 }
