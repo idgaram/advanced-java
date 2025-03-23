1215e145bb286cdef28c73b8a65f7a80532805cf
IDgaram <lorys.lellouche@gmail.com>
2025-03-23 19:29:41 -0400

    exploring methods from the stream api to understand the filter and map methods

--- Demo.java
+++ Demo.java
@@ -1,50 +1,33 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
         List<Integer> nums = Arrays.asList(4, 5, 6, 8);
 
-        int sum = 0;
+        Predicate<Integer> p = n -> n % 2 == 0;
+
+        Function<Integer, Integer> f1 = n -> n * 2;
+        // Function<Integer, Integer> f1 = new Function<Integer, Integer>() {
+
+        // @Override
+        // public Integer apply(Integer n) {
+        // {
+        // return n * 2;
+        // }
+        // }
+
+        // };
 
         int result = nums.stream()
-                .filter(n -> n % 2 == 0)
-                .map(n -> n += n)
+                .filter(p)
+                .map(f1)
                 .reduce(0, (c, e) -> c + e);
 
-        // Stream<Integer> s1 = nums.stream();
-        // Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
-        // Stream<Integer> s3 = s2.map(n -> n += n);
-        // int result = s3.reduce(0, (c, e) -> c + e);
-
         System.out.println(result);
-        // s3.forEach(n -> System.out.println(n));
 
-        // s1.filter(n -> n%2==0)
-
-        // Stream
-
-        // Consumer<Integer> cons = n ->{ if (n%2 == 0) {
-        // n += n;
-        // sum+=n;
-        // }};
-
-        // nums.forEach(cons);
-
-        // for (int i = 0; i < nums.size(); i++) {
-        // System.out.println(nums.get(i));
-        // }
-
-        // for (int n : nums) {
-
-        // if (n % 2 == 0) {
-        // n += n;
-        // sum += n;
-        // }
-        // }
-
-        // System.out.println(sum);
-
-    }
+    };
 }
