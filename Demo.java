b133635ca7e2f854d0743f6660f60029a12e6f54
IDgaram <lorys.lellouche@gmail.com>
2025-03-24 16:25:41 -0400

    optionnal making an error that is different than nullpointerexception

--- Demo.java
+++ Demo.java
@@ -1,60 +1,24 @@
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+import java.util.Optional;
 
 public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        List<Integer> nums = new ArrayList<>(10000);
+        List<String> names = Arrays.asList("benoit", "catherine", "clara", "romane", "john");
 
-        Random ran = new Random();
+        // String name = names.stream()
+        // .filter(n -> n.contains("x"))
+        // .findFirst().orElse("no name with this sequence was found");
 
-        for (int i = 0; i < 10000; i++) {
-            nums.add(ran.nextInt(100));
-        }
+        // System.out.println(name);
 
-        Long startSeq1 = System.currentTimeMillis();
+        Optional<String> name = names.stream()
+                .filter(n -> n.contains("x"))
+                .findFirst();
 
-        // int sum1 = nums.stream()
-        // .map(i -> i * 2)
-        // .reduce(0, (c, e) -> c + e);
-
-        int sum2 = nums.stream()
-                .map(i -> {
-                    try {
-                        Thread.sleep(1);
-                    } catch (Exception e) {
-
-                    }
-
-                    return i * 2;
-                })
-                .mapToInt(i -> i)
-                .sum();
-
-        Long endSeq1 = System.currentTimeMillis();
-
-        Long startSeq2 = System.currentTimeMillis();
-
-        int sum3 = nums.parallelStream()
-                .map(i -> {
-                    try {
-                        Thread.sleep(1);
-                    } catch (Exception e) {
-
-                    }
-
-                    return i * 2;
-                })
-                .mapToInt(i -> i)
-                .sum();
-
-        Long endSeq2 = System.currentTimeMillis();
-
-        // nums.forEach(n -> System.out.println(n));
-        System.out.println(sum2 + " " + sum3);
-        System.out.println("sum2 : " + (endSeq1 - startSeq1));
-        System.out.println("sum3 : " + (endSeq2 - startSeq2));
+        System.out.println(name.orElse("no name with this sequence was found"));
     };
 }
