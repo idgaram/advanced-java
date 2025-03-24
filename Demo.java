3c03f5f947c88fa9911d32ebeeaf5e9bacd9cf52
IDgaram <lorys.lellouche@gmail.com>
2025-03-23 20:14:02 -0400

    demonstration of the speed of parallelStream

--- Demo.java
+++ Demo.java
@@ -1,33 +1,60 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.Random;
 
 public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> nums = Arrays.asList(4, 5, 6, 8);
 
-        Predicate<Integer> p = n -> n % 2 == 0;
+        List<Integer> nums = new ArrayList<>(10000);
 
-        Function<Integer, Integer> f1 = n -> n * 2;
-        // Function<Integer, Integer> f1 = new Function<Integer, Integer>() {
+        Random ran = new Random();
 
-        // @Override
-        // public Integer apply(Integer n) {
-        // {
-        // return n * 2;
-        // }
-        // }
+        for (int i = 0; i < 10000; i++) {
+            nums.add(ran.nextInt(100));
+        }
 
-        // };
+        Long startSeq1 = System.currentTimeMillis();
 
-        int result = nums.stream()
-                .filter(p)
-                .map(f1)
-                .reduce(0, (c, e) -> c + e);
+        // int sum1 = nums.stream()
+        // .map(i -> i * 2)
+        // .reduce(0, (c, e) -> c + e);
 
-        System.out.println(result);
+        int sum2 = nums.stream()
+                .map(i -> {
+                    try {
+                        Thread.sleep(1);
+                    } catch (Exception e) {
 
+                    }
+
+                    return i * 2;
+                })
+                .mapToInt(i -> i)
+                .sum();
+
+        Long endSeq1 = System.currentTimeMillis();
+
+        Long startSeq2 = System.currentTimeMillis();
+
+        int sum3 = nums.parallelStream()
+                .map(i -> {
+                    try {
+                        Thread.sleep(1);
+                    } catch (Exception e) {
+
+                    }
+
+                    return i * 2;
+                })
+                .mapToInt(i -> i)
+                .sum();
+
+        Long endSeq2 = System.currentTimeMillis();
+
+        // nums.forEach(n -> System.out.println(n));
+        System.out.println(sum2 + " " + sum3);
+        System.out.println("sum2 : " + (endSeq1 - startSeq1));
+        System.out.println("sum3 : " + (endSeq2 - startSeq2));
     };
 }
